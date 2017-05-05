cbb02ce2a904d3970d84e307f77188c06d5441ec
RobinMayo <rbmayo@laposte.net>
2017-05-05 14:37:28 +0200

    pass checkpatch fils in dir module

--- module/ioctl_basics.h
+++ module/ioctl_basics.h
@@ -1,9 +1,9 @@
 #define IOC_MAGIC 'k'
 
-#define IOCTL_KILL _IOR(IOC_MAGIC, 0, int) 
-#define IOCTL_WAIT _IOR(IOC_MAGIC, 1, int) 
-#define IOCTL_MEMINFO _IOR(IOC_MAGIC, 2, int) 
-#define IOCTL_MODINFO _IOR(IOC_MAGIC, 3, int) 
+#define IOCTL_KILL _IOR(IOC_MAGIC, 0, int)
+#define IOCTL_WAIT _IOR(IOC_MAGIC, 1, int)
+#define IOCTL_MEMINFO _IOR(IOC_MAGIC, 2, int)
+#define IOCTL_MODINFO _IOR(IOC_MAGIC, 3, int)
 
 #define BUFF_SIZE 256
 #define MAX_PIDS 16
--- module/mod.c
+++ module/mod.c
@@ -1,8 +1,9 @@
 #include <linux/module.h>
 #include <linux/kernel.h>
-#include <linux/fs.h> // required for various structures related to files liked fops. 
+#include <linux/fs.h>
+/* required for various structures related to files liked fops. */
 #include <linux/semaphore.h>
-#include <linux/cdev.h> 
+#include <linux/cdev.h>
 #include <linux/ioctl.h>
 #include "ioctl_basics.h"
 #include <linux/version.h>
@@ -10,86 +11,91 @@
 
 static int Major;
 
-int open(struct inode *inode, struct file *filp) {
-	printk(KERN_INFO "Inside open \n");
+int open(struct inode *inode, struct file *filp)
+{
+	printk (KERN_INFO "Inside open \n");
 	return 0;
 }
 
-int release(struct inode *inode, struct file *filp) {
+int release(struct inode *inode, struct file *filp)
+{
 	printk (KERN_INFO "Inside close \n");
 	return 0;
 }
 
-long ioctl_funcs(struct file *filp,unsigned int cmd, unsigned long arg) {
+long ioctl_funcs(struct file *filp, unsigned int cmd, unsigned long arg)
+{
 
 	struct mesg_kill *mesg;
-	switch(cmd) {
+	switch (cmd) {
 
-	case IOCTL_KILL: 
-		printk(KERN_INFO "Asked KILL");
+	case IOCTL_KILL:
+		printk (KERN_INFO "Asked KILL");
 		copy_from_user((char *)mesg, (char *)arg, sizeof(*mesg));
-		printk(KERN_INFO "Asked KILL args r: %d & %d", mesg->signal, mesg-> pid);
+			printk(KERN_INFO "Asked KILL args r: %d & %d",
+		mesg->signal, mesg->pid);
 		break;
 
-	case IOCTL_WAIT: 
-		printk(KERN_INFO "Asked WAIT");
+	case IOCTL_WAIT:
+		printk (KERN_INFO "Asked WAIT");
 		break;
 
-	case IOCTL_MEMINFO: 
-		printk(KERN_INFO "Asked MEMINFO");
+	case IOCTL_MEMINFO:
+		printk (KERN_INFO "Asked MEMINFO");
 		break;
 
-	case IOCTL_MODINFO: 
-		printk(KERN_INFO "Asked MODINFO");
+	case IOCTL_MODINFO:
+		printk (KERN_INFO "Asked MODINFO");
 		break;
-	} 
+	}
 
 	return 0;
 }
 
 struct file_operations fops = {
- open:   open,
- unlocked_ioctl: ioctl_funcs,
- release: release
+	open: open,
+	unlocked_ioctl : ioctl_funcs,
+	release : release
 };
 
 
-struct cdev *kernel_cdev; 
+struct cdev *kernel_cdev;
 
 
-int char_arr_init (void) {
+int char_arr_init (void)
+{
 	int ret;
-	dev_t dev_no,dev;
+	dev_t dev_no, dev;
 
-	kernel_cdev = cdev_alloc(); 
+	kernel_cdev = cdev_alloc();
 	kernel_cdev->ops = &fops;
 	kernel_cdev->owner = THIS_MODULE;
 	printk (" Inside init module\n");
-	ret = alloc_chrdev_region( &dev_no , 0, 1,"char_arr_dev");
+	ret = alloc_chrdev_region(&dev_no, 0, 1, "char_arr_dev");
 
 	if (ret < 0) {
-		printk("Major number allocation is failed\n");
-		return ret; 
-	}
-
-	Major = MAJOR(dev_no);
-	dev = MKDEV(Major,0);
-	printk (" The major number for your device is %d\n", Major);
-	ret = cdev_add( kernel_cdev,dev,1);
-
-	if(ret < 0 ) {
-		printk(KERN_INFO "Unable to allocate cdev");
+		printk ("Major number allocation is failed\n");
 		return ret;
 	}
 
- return 0;
+	Major = MAJOR(dev_no);
+	dev = MKDEV(Major, 0);
+	printk (" The major number for your device is %d\n", Major);
+	ret = cdev_add(kernel_cdev, dev, 1);
+
+	if (ret < 0) {
+		printk (KERN_INFO, "Unable to allocate cdev");
+		return ret;
+	}
+	return 0;
 }
 
-void char_arr_cleanup(void) {
-	printk(KERN_INFO " Inside cleanup_module\n");
+void char_arr_cleanup(void)
+{
+	printk (KERN_INFO, " Inside cleanup_module\n");
 	cdev_del(kernel_cdev);
 	unregister_chrdev_region(Major, 1);
 }
-MODULE_LICENSE("GPL"); 
+MODULE_LICENSE("GPL");
 module_init(char_arr_init);
 module_exit(char_arr_cleanup);
